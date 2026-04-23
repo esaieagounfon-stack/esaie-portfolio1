5a86fe55659bc666045c2ae99d4ac18ff6aaf4d4
esaieagounfon-stack <esaieagounfon@gmail.com>
2026-04-24 00:38:45 +0100

    add de My AfricaChat

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
     <section id="contact" className="py-20 bg-black px-6">
       <div className="max-w-2xl mx-auto">
         <h2 className="text-4xl font-bold mb-4 text-center text-yellow-400 sm:text-5xl">Me Contacter</h2>
-        <p className="text-gray-400 mb-12 text-center text-base sm:text-lg">Vous avez un projet en tête ? Contactez-moi !</p>
+        <p className="text-gray-400 mb-12 text-center text-base sm:text-lg">Vous avez un projet? Contactez-moi !</p>
         <form className="space-y-6 bg-gray-800 p-8 rounded-lg border-2 border-green-500 sm:p-10">
           <input
             type="text"
